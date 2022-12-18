213dad904b440b7568a45bdba51a3fcec6847a68
royweb <xytank@gmail.com>
2022-12-18 11:08:21 -0500

    Preview when dragover

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - 'defer' to load the script file before HTML
 - add attribute 'draggable' to allow drag and drop
 - default cursor for 'dragover' Event Listener is disabled
+- 'querySelectorAll' does not return array
--- script.js
+++ script.js
@@ -17,8 +17,35 @@ containers.forEach(container => {
     container.addEventListener('dragover', (e) => {
         // Change the default disabled cursor to enable cursor
         e.preventDefault()
-
+        const afterElement = getDragAfterElement(container, e.clientY)
         const draggable = document.querySelector('.dragging')
-        container.appendChild(draggable)
+
+        // Add the element to the end
+        if (afterElement == null) {
+            container.appendChild(draggable)
+        }
+        else {
+            container.insertBefore(draggable, afterElement)
+        }
     })
 })
+
+function getDragAfterElement(container, y) {
+    // Get all the container but not the dragend container
+    // 'querySelectorAll' does not return array
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    // 'reduce' loop through the draggable elements and determine which single element is the closest to zero
+    return draggableElements.reduce((closest, child) => {
+        // Find the position of the element
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+        // Check if the element is above the element
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        }
+        else {
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY}).element
+}
